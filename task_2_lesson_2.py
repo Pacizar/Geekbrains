45c619187c62491d17917cc01d98a53bcfdd4b7b
Paсizar [Kraken] <pacizar85@gmail.com>
2022-03-14 10:41:14 +0300

    чистка папок

--- task_2_lesson_2.py
+++
@@ -1,6 +0,0 @@
-x_list = list(input("Введите числа: "))
-
-for x in range(1, len(x_list), 2):
-    x_list[x - 1], x_list[x] = x_list[x], x_list[x - 1]
-
-print(x_list)
--- task_4_lesson_2.py
+++
@@ -1,4 +0,0 @@
-x = input("Введите слово: ").split()
-
-for a, b in enumerate(x, 1):
-    print(a, b) if len(b) <= 10 else print(a, b[:10])
